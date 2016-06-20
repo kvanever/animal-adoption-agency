2d54364fb76da8bcac482f9921982f1edf1eb723
Kevin VanEvery and Meaghan Jones <kevin@>
2016-06-20 09:19:33 -0700

    add prototype for adoption method

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
 // Business rules for animal adoption agency
 
-var Animal = function(name, age, species, gender, color) {
+var Animal = function(name, age, species, gender, color, adopted) {
   this.name = name;
   this.age = age;
   this.species = species;
   this.gender = gender;
   this.color = color;
+  this.adopted = false;
+}
+
+Animal.prototype.adopt(adopted) {
+  this.adopted = true;
+  return this;
 }
 
 // Calls input from form-input.html
